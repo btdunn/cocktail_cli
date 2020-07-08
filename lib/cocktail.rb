39569b121a43f58147940fde91a894329092ddd5
Ben Dunn <benthedunn@gmail.com>
2020-07-08 13:06:47 -0600

    adjust sleep in quiz

--- lib/cocktail.rb
+++ lib/cocktail.rb
@@ -3,7 +3,7 @@ class Cocktail < ActiveRecord::Base
 
   def self.drink_quiz
     puts "Time to pick a drink!"
-    sleep(1.5)
+    sleep(1)
     puts "Pick your poison!"
     baseSpirit = gets.strip
     puts "What time of day is it?"
