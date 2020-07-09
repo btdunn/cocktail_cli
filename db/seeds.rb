2411233b44b053a88fb5dc0d89b325857a9d7b7a
Bmcalkins <b.raders1013@gmail.com>
2020-07-09 16:23:45 -0600

    menu update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 
 Cocktail.destroy_all
 
-$cool_drink = Cocktail.create(name: "Cool Drink", baseSpirit: "rum", time: "day", weather: "sunny", ingredients: "just rum lol")
+$cool_drink = Cocktail.create(name: "Cool Drink", baseSpirit: "rum", time: "morning", weather: "hot", ingredients: "just rum lol")
